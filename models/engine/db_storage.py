2f2f515aade43be4f6eef91050bf1a2a7805d033
belindaHBTN <6372@holbertonstudents.com>
2023-08-03 21:56:01 -0700

    6-4. New engine DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,6 +3,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class DBStorage():
@@ -25,10 +31,11 @@ class DBStorage():
             Base.metadata.drop_all(self.__engine)
 
     def all(self,cls=None):
-        Session = sessionmaker(bind=engine)
-        self.__session = Session(cls)
+        """Return a dictionary of models currently in database"""
+#        Session = sessionmaker(bind=self.__engine)
+#        self.__session = Session(cls)
         if cls is None:
-            query = self.__session.query(User, State, City, Amenity, Review).all()
+            query = self.__session.query(User, State, City, Place, Amenity, Review).all()
         else:
             query = self.__session.query(cls).all()
         temp_dict = {}
@@ -38,3 +45,24 @@ class DBStorage():
             key = class_name + "." + obj_id
             temp_dict[key] = obj
         return temp_dict
+
+
+    def new(self, obj):
+        """Add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
