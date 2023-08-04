78b33a814fc97e0351f7b2f186efb3e8765ceae6
angellalao <6369@holbertonstudents.com>
2023-08-04 04:42:58 -0700

    establish relationships

--- models/city.py
+++ models/city.py
@@ -11,3 +11,6 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    places = relationship("Place", back_populates="city",
+                          cascade="all, delete, delete-orphan")
