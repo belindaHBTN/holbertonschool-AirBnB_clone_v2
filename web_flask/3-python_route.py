81b6be281e582e0c7be684f8d623a0b9db8b6e29
belindaHBTN <6372@holbertonstudents.com>
2023-08-14 05:32:42 -0700

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web app with routes"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function called with / route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function called with /hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Function called with /c/<text> route"""
+    return "C" + " " + text.replace("_", " ")
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Function called with /python/ or /python/<text> route"""
+    return "Python" + " " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
