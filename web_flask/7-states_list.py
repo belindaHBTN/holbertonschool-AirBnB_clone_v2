6f55daf61a0d6e83b0a5dcf70121bb1f9b372403
belindaHBTN <6372@holbertonstudents.com>
2023-08-27 06:50:27 -0700

    updated list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,25 +2,26 @@
 """A script that starts a Flask web app with routes"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Function called with /states_list route"""
-    state_dict = storage.all("State")
-    state_list = []
-    for state in state_dict.values():
-        state_list.append(state)
-    return render_template('7-states_list.html', state_list=state_list)
-
-
 @app.teardown_appcontext
-def fetch_data():
+def teardown(exc):
     """remove the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Retrieves the list of all State objects"""
+    state_dict = storage.all(State)
+    state_list = []
+    for state in state_dict.values():
+        state_list.append(state.to_dict())
+    return render_template('7-states_list.html', state_list=state_list)
 
 
 if __name__ == '__main__':
